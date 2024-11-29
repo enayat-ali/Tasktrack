6920e3bb97be5e189752f030b26358693a922312
Enayat Ali <39117929+enayat-ali@users.noreply.github.com>
2024-11-29 14:08:31 +0530

    update script.js

--- script.js
+++ script.js
@@ -5,15 +5,23 @@ const addBtn = document.querySelector(".addBtn");
 function addItem() {
     const taskText = searchTask.value.trim();
     if (taskText !== ""){
-        const addTask = document.querySelector(".addTask")
+        const addTask = document.querySelector(".addTask");
         const listItem = document.createElement("li");
         listItem.innerHTML =` <p> ${taskText} </p> 
         <button type="submit" class="done btn"><img src="check.png" alt=""/></button>
-        <button type="submit" class="done btn"><img src="bin.png" alt="" /></button>
-        <button type="submit" class="done btn"><img src="pen.png" alt="" /></button>`;
+        <button type="submit" class="dlt btn"><img src="bin.png" alt="" /></button>
+        <button type="submit" class="edit btn"><img src="pen.png" alt="" /></button>`;
 
         addTask.appendChild(listItem);
         searchTask.value = '';
+        const remove = listItem.querySelector(".dlt");
+         remove.addEventListener("click", function() {
+          addTask.removeChild(listItem)
+        })
+        const done = listItem.querySelector(".done");
+        done.addEventListener("click", function() {
+          listItem.style.textDecoration = "underline"
+        })
     }
 }
 
